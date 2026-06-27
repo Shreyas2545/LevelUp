5701aaa2a2d4bad80460a7380bf4116ccb9a7abc
Shreyas2545 <shreyashadawale25@gmail.com>
2026-06-27 14:53:44 +0530

    revise func & js-methods

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+
+let a = "Hello Shreyas"
+let ans = `Hey there , ${a} , ...Thanks for visiting us !`
+console.log(ans);
+
+// const obj ={
+//     name : 'Pen',
+//     price : 20,
+//     valueable : false,
+// };
+// obj.valueable = true;
+// obj.color = "green";
+// for (let i in obj){
+//     console.log(i,obj[i]);
+// }
+
+// let str ='Shreyas';
+// let ans = str.length;
+// console.log(ans);
+
+
+// let string = "M  Y"
+
+// for (let i in string){
+//     console.log(i);
+    
+// }
+
+// let ans = confirm("Type here")
+// console.log(typeof ans);
+
+
+// let age = 17;
+// let result = age > 18 ? "you can vote " : "you cant vote";
+// console.log(result);
+
+// let a = 10;
+// let b = "10";
+
+// if(a === b){
+//     alert("true");
+// }
+// else{
+//     alert("false");
+// }
+
 // let ans = confirm("Are u married ?");
 
 // if(ans){
@@ -7,8 +53,12 @@
 //     alert("You aren't a married person !")
 // }
 
- 
 
+
+// let a =1 ;
+// while(a == 1){
+//     alert("Stucked in loop")
+// }
 
 // let name = prompt("Whats ur name ?");
 // console.log("Name is " + name);
