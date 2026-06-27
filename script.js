bebb3fdab8e6d4d2a38eaaf25f80ec2c5ca4e327
Shreyas2545 <shreyashadawale25@gmail.com>
2026-06-27 14:20:21 +0530

    pop-ups

---
+++ script.js
@@ -0,0 +1,18 @@
+// let ans = confirm("Are u married ?");
+
+// if(ans){
+//     alert("You are a married person !")
+// }
+// else {
+//     alert("You aren't a married person !")
+// }
+
+ 
+
+
+// let name = prompt("Whats ur name ?");
+// console.log("Name is " + name);
+
+
+// confirm("Are you there Shreyas ?")
+// alert("Hello Shreyas");
