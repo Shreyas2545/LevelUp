daac77df405552b7e3791f12d84f7c6d903f8059
Shreyas2545 <shreyashadawale25@gmail.com>
2026-06-29 20:03:18 +0530

    selector and DOM

--- script.js
+++ script.js
@@ -1,145 +1,161 @@
+// let a = document.querySelectorAll("h4");
+// console.log(a);
 
 
-// let arr =[1,2,3,4,5,6,7,8,9,10];
-// let newarr = arr.reduce((result,current)=>{
-//     return result+current ;
-// });
-// console.log(newarr);
+// let a = document.getElementsByTagName("p");
+// console.dir(a);
 
 
-// let arr = [1,2,3,4,5,6,7,8,9,10];
-// let newarr = arr.filter((value)=>{
-//     return value%2 === 0 ;
-// });
-// console.log(newarr);
+// const obj = {
+//     name : "Shreyas",
+//     age : 18 ,
+//     married : false,
+// };
 
-// / let num = [1,2,3,4,5];
-// let newnum  = num.map((value)=>{
-//     return value+10;
-// });
-
-// console.log(newnum);
+// console.log(document);   
+// console.log(obj);
 
 
+// // let arr =[1,2,3,4,5,6,7,8,9,10];
+// // let newarr = arr.reduce((result,current)=>{
+// //     return result+current ;
+// // console.log(newarr);  
+// // });
 
-// let arr =[10,20,30,40,50];
-// arr.forEach((value,index)=>{
-//     console.log("Value is : " + value, "Index is : " + index);
+
+// // let arr = [1,2,3,4,5,6,7,8,9,10];
+// // let newarr = arr.filter((value)=>{
+// //     return value%2 === 0 ;
+// // });
+// // console.log(newarr);
+
+// // / let num = [1,2,3,4,5];
+// // let newnum  = num.map((value)=>{
+// //     return value+10;
+// // });
+
+// // console.log(newnum);
+
+
+
+// // let arr =[10,20,30,40,50];
+// // arr.forEach((value,index)=>{
+// //     console.log("Value is : " + value, "Index is : " + index);
     
-// })
+// // })
 
-// const sum_num = (num1 , num2) =>{
-//     // console.log(num1 + num2);
-//     return num1 + num2 ;
-// };
-
-// let result = sum_num(10,20)
-// console.log(result);
-
-// const arrow_function = (para1,para2)=> {
-//     console.log(`Name is ${para1} and sirname is ${para2}`);
-// };
-
-// arrow_function("Leonel","Messi");
-
-// function parameter_usage (message){
-//     console.log(message);
-// };
-
-// parameter_usage("Hello shreyas");
-
-// function msg(){
-//     console.log("hello shreyas");
-// }
-// msg();
-
-
-
-// let arr = ["shreyas","kunal","rahul","narendra","keshav"];
-// let stringconverter = arr.toString() ;
-// console.log(stringconverter);
-
-
-// let arr = ["shreyas","kunal","rahul","narendra","keshav"];
-// // arr[0] = "SHREYAS";
-// // console.log(arr);
-// // console.log(arr[2]);
-
-// arr.push("Abhir","Shrushti");
-// arr.pop();
-// arr.pop();
-// arr.pop();
-// arr.pop();
-
-// for (let i of arr){
-//     console.log(i);
-    
-// }
-
-// // let a = "Hello Shreyas"
-// // let ans = `Hey there , ${a} , ...Thanks for visiting us !`
-// // console.log(ans);
-
-// // const obj ={
-// //     name : 'Pen',
-// //     price : 20,
-// //     valueable : false,
+// // const sum_num = (num1 , num2) =>{
+// //     // console.log(num1 + num2);
+// //     return num1 + num2 ;
 // // };
-// // obj.valueable = true;
-// // obj.color = "green";
-// // for (let i in obj){
-// //     console.log(i,obj[i]);
+
+// // let result = sum_num(10,20)
+// // console.log(result);
+
+// // const arrow_function = (para1,para2)=> {
+// //     console.log(`Name is ${para1} and sirname is ${para2}`);
+// // };
+
+// // arrow_function("Leonel","Messi");
+
+// // function parameter_usage (message){
+// //     console.log(message);
+// // };
+
+// // parameter_usage("Hello shreyas");
+
+// // function msg(){
+// //     console.log("hello shreyas");
 // // }
-
-// // let str ='Shreyas';
-// // let ans = str.length;
-// // console.log(ans);
+// // msg();
 
 
-// // let string = "M  Y"
 
-// // for (let i in string){
+// // let arr = ["shreyas","kunal","rahul","narendra","keshav"];
+// // let stringconverter = arr.toString() ;
+// // console.log(stringconverter);
+
+
+// // let arr = ["shreyas","kunal","rahul","narendra","keshav"];
+// // // arr[0] = "SHREYAS";
+// // // console.log(arr);
+// // // console.log(arr[2]);
+
+// // arr.push("Abhir","Shrushti");
+// // arr.pop();
+// // arr.pop();
+// // arr.pop();
+// // arr.pop();
+
+// // for (let i of arr){
 // //     console.log(i);
     
 // // }
 
-// // let ans = confirm("Type here")
-// // console.log(typeof ans);
+// // // let a = "Hello Shreyas"
+// // // let ans = `Hey there , ${a} , ...Thanks for visiting us !`
+// // // console.log(ans);
+
+// // // const obj ={
+// // //     name : 'Pen',
+// // //     price : 20,
+// // //     valueable : false,
+// // // };
+// // // obj.valueable = true;
+// // // obj.color = "green";
+// // // for (let i in obj){
+// // //     console.log(i,obj[i]);
+// // // }
+
+// // // let str ='Shreyas';
+// // // let ans = str.length;
+// // // console.log(ans);
 
 
-// // let age = 17;
-// // let result = age > 18 ? "you can vote " : "you cant vote";
-// // console.log(result);
+// // // let string = "M  Y"
 
-// // let a = 10;
-// // let b = "10";
+// // // for (let i in string){
+// // //     console.log(i);
+    
+// // // }
 
-// // if(a === b){
-// //     alert("true");
-// // }
-// // else{
-// //     alert("false");
-// // }
-
-// // let ans = confirm("Are u married ?");
-
-// // if(ans){
-// //     alert("You are a married person !")
-// // }
-// // else {
-// //     alert("You aren't a married person !")
-// // }
+// // // let ans = confirm("Type here")
+// // // console.log(typeof ans);
 
 
+// // // let age = 17;
+// // // let result = age > 18 ? "you can vote " : "you cant vote";
+// // // console.log(result);
 
-// // let a =1 ;
-// // while(a == 1){
-// //     alert("Stucked in loop")
-// // }
+// // // let a = 10;
+// // // let b = "10";
 
-// // let name = prompt("Whats ur name ?");
-// // console.log("Name is " + name);
+// // // if(a === b){
+// // //     alert("true");
+// // // }
+// // // else{
+// // //     alert("false");
+// // // }
+
+// // // let ans = confirm("Are u married ?");
+
+// // // if(ans){
+// // //     alert("You are a married person !")
+// // // }
+// // // else {
+// // //     alert("You aren't a married person !")
+// // // }
 
 
-// // confirm("Are you there Shreyas ?")
-// // alert("Hello Shreyas");
+
+// // // let a =1 ;
+// // // while(a == 1){
+// // //     alert("Stucked in loop")
+// // // }
+
+// // // let name = prompt("Whats ur name ?");
+// // // console.log("Name is " + name);
+
+
+// // // confirm("Are you there Shreyas ?")
+// // // alert("Hello Shreyas");
